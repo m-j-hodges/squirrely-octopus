bbe90653a80f70a421fe7b3d5970849857ff3366
Matthew Hodges <matt.j.hodges@gmail.com>
2022-10-05 20:02:45 -0700

    small changes

--- myapp/src/components/NavTabs.js
+++ myapp/src/components/NavTabs.js
@@ -39,7 +39,6 @@ return (
   </nav>
   <div className="p-5 text-center bg-light">
     <h1 className="mb-3">Welcome to my Portfolio!</h1>
-    <h4 className="mb-3">Subheading</h4>
   </div>
 </div>
 )
--- myapp/src/components/footer.js
+++ myapp/src/components/footer.js
@@ -7,9 +7,8 @@ function Footer() {
     <div>
     <p> This website was built my Matthew Hodges</p>
     <p> Contact:</p>
-    <p>email: matt.j.hodges@icloud.com</p>
-    <p>linkedIn: https://www.linkedin.com/in/matthew-hodges-23395b67/</p>
-    <p> thank you for visiting</p>
+    <a className="d-block" href="mailto:matt.j.hodges@icloud.com"> email me!</a>
+    <a href="https://www.linkedin.com/in/matthew-hodges-23395b67/">LinkedIn </a>
     </div>
 
   )
