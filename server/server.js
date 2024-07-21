a0d6b8888cfcc73ae876eaf737b5578e2f600f70
Matthew Hodges <matt.j.hodges@gmail.com>
2024-07-21 11:09:47 -0700

    checking to see if this resolves invalid host headers

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(cors());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, UPDATE, PATCH');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
